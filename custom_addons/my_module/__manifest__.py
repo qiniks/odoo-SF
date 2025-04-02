3d32b40be560aae752dbc5bcd0e133103dd66a17
Baiel11 <baielzhamalidinov@gmail.com>
2025-04-02 12:52:34 +0600

    commit

--- custom_addons/my_module/__manifest__.py
+++ custom_addons/my_module/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Custom Sale and Inventory Extension",
+    "name": "Custom Sale and Inventory Extensn",
     "version": "1.0",
     "depends": ["sale", "stock"],
     "data": [
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+# Check if PASSWORD_FILE is set and read it
 if [ -v PASSWORD_FILE ]; then
     PASSWORD="$(< $PASSWORD_FILE)"
 fi
 
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the odoo process if not present in the config file
+# Set the PostgreSQL database host, port, user, and password according to the environment
+# and pass them as arguments to the Odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
@@ -23,6 +24,7 @@ function check_config() {
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
+
 check_config "db_host" "$HOST"
 check_config "db_port" "$PORT"
 check_config "db_user" "$USER"
@@ -34,8 +36,12 @@ case "$1" in
         if [[ "$1" == "scaffold" ]] ; then
             exec odoo "$@"
         else
+            # Wait for PostgreSQL and start Watchdog to automatically restart Odoo on Python file changes
             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+            echo "Starting watchmedo process"
+            watchmedo auto-restart --recursive --pattern="*.py" -- bash -c "odoo --dev all" &
             exec odoo "$@" "${DB_ARGS[@]}"
+            echo "watchmedo started"
         fi
         ;;
     -*)
@@ -44,6 +50,61 @@ case "$1" in
         ;;
     *)
         exec "$@"
+        ;;
 esac
 
 exit 1
+
+
+
+
+
+# #!/bin/bash
+
+# set -e
+
+# if [ -v PASSWORD_FILE ]; then
+#     PASSWORD="$(< $PASSWORD_FILE)"
+# fi
+
+# # set the postgres database host, port, user and password according to the environment
+# # and pass them as arguments to the odoo process if not present in the config file
+# : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+# : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+# : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+# : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+# DB_ARGS=()
+# function check_config() {
+#     param="$1"
+#     value="$2"
+#     if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+#         value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+#     fi;
+#     DB_ARGS+=("--${param}")
+#     DB_ARGS+=("${value}")
+# }
+# check_config "db_host" "$HOST"
+# check_config "db_port" "$PORT"
+# check_config "db_user" "$USER"
+# check_config "db_password" "$PASSWORD"
+
+# case "$1" in
+#     -- | odoo)
+#         shift
+#         if [[ "$1" == "scaffold" ]] ; then
+#             exec odoo "$@"
+#         else
+#             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+#             exec odoo "$@" "${DB_ARGS[@]}"
+#         fi
+#         ;;
+#     -*)
+#         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+#         exec odoo "$@" "${DB_ARGS[@]}"
+#         ;;
+#     *)
+#         exec "$@"
+# esac
+
+# exit 1
